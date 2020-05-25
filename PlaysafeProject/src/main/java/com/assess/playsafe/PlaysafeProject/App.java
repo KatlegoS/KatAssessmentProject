38df1d7964dee05f549ec11acbf4db367d251844
katmane <shiane.katlego@gmail.com>
2020-05-25 19:27:48 +0200

    Initial project setup
    Initial project setup: maven project creation

---
+++ PlaysafeProject/src/main/java/com/assess/playsafe/PlaysafeProject/App.java
@@ -0,0 +1,13 @@
+package com.assess.playsafe.PlaysafeProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
