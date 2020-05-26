037f21e6e1ae2e93d2ce200ea6d8f0f9ec5c8368
katmane <shiane.katlego@gmail.com>
2020-05-26 10:34:44 +0200

    Error handling
    Added error handling

--- PlaysafeProject/src/main/java/com/assess/playsafe/PlaysafeProject/service/impl/ConversionServiceImpl.java
+++ PlaysafeProject/src/main/java/com/assess/playsafe/PlaysafeProject/service/impl/ConversionServiceImpl.java
@@ -6,21 +6,30 @@ public class ConversionServiceImpl implements ConversionService {
 
 	public double kelvinToCelcius(double kelvin) {
 
+		if(kelvin != 0) {
 		double celcius = kelvin - 273.0;
-		
 		return celcius;
+	}else {
+		throw new RuntimeException("Kelvin cannot be null or 0");
+	}
 	}
 
 	public double poundsToKilograms(double pounds) {
+		if(pounds != 0) {
 		double kilo = pounds * 0.45;
-		
 		return kilo;
+		}else {
+			throw new RuntimeException("pounds cannot be null or 0");
+		}
 	}
 
 	public double milesToKilometers(double miles) {
+		if(miles != 0) {
 		double kilometers = miles * 1.60934;
-		
 		return kilometers;
+		}else {
+			throw new RuntimeException("miles cannot be null or 0");
+		}
 	}
 
 	
